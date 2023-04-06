416d437f07a84f6320e2836ae4515a3588ce865d
KorbutViacheslav <minowaman777@gmail.com>
2023-04-06 20:11:32 +0300

    Add to git hub homework_26_web_servlet.

---
+++ src/main/java/com/example/webservlet/entity/Order.java
@@ -0,0 +1,19 @@
+package com.example.webservlet.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private Long id;
+    private LocalDateTime date;
+    private BigDecimal cost;
+    private List<Product> products;
+}
